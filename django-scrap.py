c63c7ed7188f2fb162140940187c7989db294a2e
John Glatts <33476958+jdglatts@users.noreply.github.com>
2018-07-21 13:35:35 -0400

    Add files via upload

---
+++ django-scrap.py
@@ -0,0 +1,51 @@
+# ------------------ 
+#
+#
+# Script to find django- jobs. Data from django's website.
+#
+#
+# ------------------ 
+
+import os 
+import webbrowser
+
+import requests
+from bs4 import BeautifulSoup
+
+
+
+def scrapGuy():
+	# Extract data from Drudge
+		try:
+			print('View Django-Jobs?')
+			search = input()   
+			
+			if search == 'yes':
+				url = 'https://www.djangoproject.com/community/jobs/' 
+
+				# add header
+				headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
+				source_code = requests.get(url, headers = headers) # pass the url to requests, HTTP for humans
+				plain_text = source_code.text 
+				soup = BeautifulSoup(plain_text, "html.parser") # pass/parse the url with bs4
+
+				# playing w/ bs4 in 
+				for jobs in soup.find_all("ul",{"class":"list-news"}):
+					print(jobs.get_text())
+					print('\n')
+
+				#second check to see if we should open in browser
+				print('\n Open This Search In Browser?')
+				check = input()
+				if check == 'yes':
+					webbrowser.open(url)
+				else:
+					print('\n\n\n\tProgram Canceled\n\n\n')
+			else:
+				print('OK!')
+
+		except  KeyboardInterrupt:
+			print('\n\n\n\tProgram Canceled\n\n\n')
+
+
+scrapGuy()
